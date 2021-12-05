e7f0b2be553a5135d7ff46c16dd64dcdf3c0ddee
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-05 17:30:33 +0700

    Edit UKM id

--- app/Models/UkmModel.php
+++ app/Models/UkmModel.php
@@ -7,7 +7,7 @@ use CodeIgniter\Model;
 class UkmModel extends Model
 {
     protected $table = 'ukm';
-    protected $primarykey = 'id_ukm';
+    protected $primarykey = 'id';
     protected $useTimestamps = true;
     protected $allowedFields = ['nama', 'deskripsi', 'kategori', 'logo', 'jadwal', 'tempat', 'ketua', 'angkatan_ketua', 'wakil', 'angkatan_wakil', 'pencapaian', 'created_at', 'updated_at'];
 }
