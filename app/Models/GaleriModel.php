94b2987f6888cf85b1f1d4ee346801280b291f4a
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-05 17:36:37 +0700

    Edit galeri id

--- app/Models/GaleriModel.php
+++ app/Models/GaleriModel.php
@@ -7,7 +7,7 @@ use CodeIgniter\Model;
 class GaleriModel extends Model
 {
     protected $table = 'galeriukm';
-    protected $primarykey = 'id_galeri';
+    protected $primarykey = 'id';
     protected $useTimestamps = true;
     protected $allowedFields = ['id_ukm', 'foto', 'created_at'];
 }
