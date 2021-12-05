8e1e54b61fb9d06a3296f0c7d9e5e1692eec1826
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-05 17:26:50 +0700

    Edit kalender id

--- app/Models/KalenderModel.php
+++ app/Models/KalenderModel.php
@@ -7,7 +7,7 @@ use CodeIgniter\Model;
 class KalenderModel extends Model
 {
     protected $table = 'kalender';
-    protected $primarykey = 'id_kalender';
+    protected $primarykey = 'id';
     protected $useTimestamps = true;
     protected $allowedFields = ['nama', 'kategori', 'tanggal', 'kegiatan', 'keterangan', 'created_at', 'updated_at'];
 }
