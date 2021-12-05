b3348fbf5f423a79f3e19f647254eaa9cccfa252
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-05 17:39:52 +0700

    Edit bidang id

--- app/Models/BidangModel.php
+++ app/Models/BidangModel.php
@@ -7,7 +7,7 @@ use CodeIgniter\Model;
 class BidangModel extends Model
 {
     protected $table = 'bidang';
-    protected $primarykey = 'id_bidang';
+    protected $primarykey = 'id';
     protected $useTimestamps = true;
     protected $allowedFields = ['nama', 'profil', 'deskripsi', 'tujuan', 'created_at', 'updated_at'];
 }
