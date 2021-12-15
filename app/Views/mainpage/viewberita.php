f0ffa389cc6e25f0ce4959059df7eee3d81d47f2
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-15 14:52:04 +0700

    Edit view berita page

--- app/Views/mainpage/viewberita.php
+++ app/Views/mainpage/viewberita.php
@@ -18,56 +18,69 @@
 		<div class="body__section">
 			<div class="isi__berita__body row">
 				<!-- info isi__berita main -->
-				<div class="isi__berita__main col-lg- col-md-9 ps-lg-0">
+				<div class="isi__berita__main col-lg- col-md-8 ps-lg-0">
 					<div class="title__section title__isi__berita text-left">
-						<h3>Prediksi Terbaru Menkes Kapan RI Capai Puncak Corona</h3>
+						<h3>Prediksi Terbaru Menkes
+							Kapan RI Capai Puncak Corona</h3>
 						<div class="keterangan__berita">
 							<i class="far fa-calendar-alt"></i> Kamis, 2/12/2021
 						</div>
 					</div> <!-- end Info isi__berita title-->
 					<div class="isi__berita__image mb-4">
-						<img class="img-fluid rounded-3 isi__berita__image__sec" src="/img/berita/news-1.jpg" alt="">
+						<img class="img-fluid rounded-3 isi__berita__image__sec" src="/mainpage/images/news-3.jpg" alt="">
 					</div>
 					<div class="isi__berita__content">
-						<p>
-							Menteri Kesehatan Budi Gunadi Sadikin menyebut kemungkinan puncak lonjakan kasus COVID-19 di Indonesia terjadi pada pertengahan Juli 2021. Prediksi ini berdasarkan pengalaman Indonesia dalam menghadapi gelombang pertama Corona.
+						<p>Menteri Kesehatan Budi Gunadi Sadikin menyebut kemungkinan puncak lonjakan kasus COVID-19
+							di Indonesia terjadi pada pertengahan Juli 2021. Prediksi ini berdasarkan pengalaman
+							Indonesia dalam menghadapi gelombang pertama Corona.
 						</p>
 						<p>
-							"Berdasarkan pengalaman kita di first wave, model yang kita gunakan itu menunjukkan akan terjadi di minggu pertama atau kedua Juli peak-nya," kata Menkes Budi dalam rapat bersama Komisi IX DPR RI, Selasa (13/7/2021).
+							"Berdasarkan pengalaman kita di first wave, model yang kita gunakan itu menunjukkan akan
+							terjadi di minggu pertama atau kedua Juli peak-nya," kata Menkes Budi dalam rapat
+							bersama Komisi IX DPR RI, Selasa (13/7/2021).
 						</p>
 						<p>
-							"Tapi, begitu ketika kita melihat kenaikannya cepat sekali dalam 2 minggu sesudah lebaran, kita melakukan rekalkulasi berdasarkan data-data yang ada di India, karena India juga kenaikannya cepat sekali," lanjutnya.
+							"Tapi, begitu ketika kita melihat kenaikannya cepat sekali dalam 2 minggu sesudah
+							lebaran, kita melakukan rekalkulasi berdasarkan data-data yang ada di India, karena
+							India juga kenaikannya cepat sekali," lanjutnya.
 						</p>
 						<p>
-							Baca artikel detikHealth, "Prediksi Terbaru Menkes Kapan RI Capai Puncak Corona" selengkapnya https://health.detik.com/berita-detikhealth/d-5642056/prediksi-terbaru-menkes-kapan-ri-capai-puncak-corona.
+							Baca artikel detikHealth, "Prediksi Terbaru Menkes Kapan RI Capai Puncak Corona"
+							selengkapnya
+							https://health.detik.com/berita-detikhealth/d-5642056/prediksi-terbaru-menkes-kapan-ri-capai-puncak-corona.
 						</p>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed incidunt saepe corrupti,nihil nostrum illum id impedit eaque eligendi quod molestias architecto dolorum? Dolores soluta officia rerum voluptates ut totam?
+
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed incidunt saepe corrupti,
+							nihil nostrum illum id impedit eaque eligendi quod molestias architecto dolorum? Dolores
+							soluta officia rerum voluptates ut totam?
 						</p>
-						<p>
-							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis enim optio minima illo hic unde, pariatur animi, rem amet ex magnam quae, maiores voluptatum. Itaque asperiores nemo dolorum quos vero.m
+						<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis enim optio minima illo
+							hic unde, pariatur animi, rem amet ex magnam quae, maiores voluptatum. Itaque asperiores
+							nemo dolorum quos vero.m
 						</p>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ipsum vero quis sint alias tenetur veniam magnam ipsa minus, quo nemo vel hic consectetur ratione neque iusto et, dignissimos illum!
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ipsum vero quis sint alias
+							tenetur veniam magnam ipsa minus, quo nemo vel hic consectetur ratione neque iusto et,
+							dignissimos illum!
 						</p>
 					</div>
 					<!--end isi berita content-->
 				</div>
-				<!-- Right Bar -->
-				<div class="isi__berita__aside__content col-lg-3 col-md-3 pe-lg-0 pt-2">
+				<div class="isi__berita__aside__content col-lg-3 col-md-4 pe-lg-0 pt-2">
 					<div class="position-sticky isi__berita__aside__sticky">
 						<h5>Berita dan informasi Populer</h5>
 						<div class="isi__berita__populer__aside">
 							<div class="berita__populer__card p-1 row justify-content-center">
 								<a href="berita.html" class="populer__card p-0 row">
-									<div class="populer__photo col-5 p-0 my-auto">
+									<div class="populer__photo col-3 col-md-5 p-0 my-auto">
 										<img class="img-fluid p-1 populer__photo__sec" src="/mainpage/images/news-3.jpg" alt="">
-										<!-- <div class="populer__img__sec" style="background-image: url(/mainpage/images/news-3.jpg);"></div> -->
+										<!-- <div class="populer__img__sec"
+													style="background-image: url(/mainpage/images/news-3.jpg);"></div> -->
 									</div>
 									<!-- end info beasiswa photo -->
-									<div class="berita__populer__card__content col-7 p-1 my-auto">
+									<div class="berita__populer__card__content col-8 col-md-7 p-1 my-auto">
 										<div class="populer__card__title py-auto">
-											<h6>Prediksi Terbaru Menkes Kapan RI Capai Puncak Corona</h6>
+											<h6>Prediksi Terbaru Menkes
+												Kapan RI Capai Puncak Corona</h6>
 										</div>
 									</div> <!-- end info beasiswa content -->
 								</a> <!-- end info beasiswa card -->
@@ -77,14 +90,16 @@
 						<div class="isi__berita__populer__aside">
 							<div class="berita__populer__card p-1 row justify-content-center">
 								<a href="berita.html" class="populer__card p-0 row">
-									<div class="populer__photo col-5 p-0 my-auto">
+									<div class="populer__photo col-3 col-md-5 p-0 my-auto">
 										<img class="img-fluid p-1 populer__photo__sec" src="/mainpage/images/news-1.jpg" alt="">
-										<!-- <div class="populer__img__sec" style="background-image: url(/mainpage/images/news-3.jpg);"></div> -->
+										<!-- <div class="populer__img__sec"
+													style="background-image: url(/mainpage/images/news-3.jpg);"></div> -->
 									</div>
 									<!-- end info beasiswa photo -->
-									<div class="berita__populer__card__content col-7 p-1 my-auto">
+									<div class="berita__populer__card__content col-8 col-md-7 p-1 my-auto">
 										<div class="populer__card__title py-auto">
-											<h6>Prediksi Terbaru Menkes Kapan RI Capai Puncak Corona</h6>
+											<h6>Prediksi Terbaru Menkes
+												Kapan RI Capai Puncak Corona</h6>
 										</div>
 									</div> <!-- end info beasiswa content -->
 								</a> <!-- end info beasiswa card -->
@@ -94,14 +109,16 @@
 						<div class="isi__berita__populer__aside">
 							<div class="berita__populer__card p-1 row justify-content-center">
 								<a href="berita.html" class="populer__card p-0 row">
-									<div class="populer__photo col-5 p-0 my-auto">
+									<div class="populer__photo col-3 col-md-5 p-0 my-auto">
 										<img class="img-fluid p-1 populer__photo__sec" src="/mainpage/images/news-2.jpg" alt="">
-										<!-- <div class="populer__img__sec" style="background-image: url(/mainpage/images/news-3.jpg);"></div> -->
+										<!-- <div class="populer__img__sec"
+													style="background-image: url(/mainpage/images/news-3.jpg);"></div> -->
 									</div>
 									<!-- end info beasiswa photo -->
-									<div class="berita__populer__card__content col-7 p-1 my-auto">
+									<div class="berita__populer__card__content col-8 col-md-7 p-1 my-auto">
 										<div class="populer__card__title py-auto">
-											<h6> Rekor Positif Covid-19 Meroket, Tembus 54.517 Kasus</h6>
+											<h6> Rekor Positif Covid-19 Meroket, Tembus 54.517 Kasus
+											</h6>
 										</div>
 									</div> <!-- end info beasiswa content -->
 								</a> <!-- end info beasiswa card -->
@@ -111,14 +128,16 @@
 						<div class="isi__berita__populer__aside">
 							<div class="berita__populer__card p-1 row justify-content-center">
 								<a href="berita.html" class="populer__card p-0 row">
-									<div class="populer__photo col-5 p-0 my-auto">
+									<div class="populer__photo col-3 col-md-5 p-0 my-auto">
 										<img class="img-fluid p-1 populer__photo__sec" src="/mainpage/images/news-3.jpg" alt="">
-										<!-- <div class="populer__img__sec" style="background-image: url(/mainpage/images/news-3.jpg);"></div> -->
+										<!-- <div class="populer__img__sec"
+													style="background-image: url(/mainpage/images/news-3.jpg);"></div> -->
 									</div>
 									<!-- end info beasiswa photo -->
-									<div class="berita__populer__card__content col-7 p-1 my-auto">
+									<div class="berita__populer__card__content col-8 col-md-7 p-1 my-auto">
 										<div class="populer__card__title py-auto">
-											<h6> Fitur Pengukur Suhu Tubuh iPhone Terbaru Bakal Hadir</h6>
+											<h6> Fitur Pengukur Suhu Tubuh iPhone Terbaru Bakal Hadir
+											</h6>
 										</div>
 									</div> <!-- end info beasiswa content -->
 								</a> <!-- end info beasiswa card -->
