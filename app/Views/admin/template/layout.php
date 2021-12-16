4a96914957e3496d7ab37be862d04d1b2f652d97
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-17 05:01:02 +0700

    Fixing layout

--- app/Views/admin/template/layout.php
+++ app/Views/admin/template/layout.php
@@ -86,209 +86,6 @@
                             </a>
                         </li>
 
-                        <!-- Messages -->
-                        <li class="nav-item dropdown hidden-caret">
-                            <a class="nav-link dropdown-toggle" href="#" id="messageDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fa fa-envelope"></i>
-                                <span class="notification">4</span>
-                            </a>
-                            <ul class="dropdown-menu messages-notif-box animated fadeIn" aria-labelledby="messageDropdown">
-                                <li>
-                                    <div class="dropdown-title d-flex justify-content-between align-items-center">
-                                        Messages
-                                        <!-- <a href="#" class="small">Mark all as read</a> -->
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="message-notif-scroll scrollbar-outer">
-                                        <div class="notif-center">
-                                            <a href="#">
-                                                <div class="notif-img">
-                                                    <img src="/admin/assets/img/jm_denis.jpg" alt="Img Profile" />
-                                                </div>
-                                                <div class="notif-content">
-                                                    <!-- Nama -->
-                                                    <span class="subject">Jimmy Denis</span>
-                                                    <!-- Subjek -->
-                                                    <span class="block"> How are you ? </span>
-                                                    <!-- Waktu Tutorial : https://www.dumetschool.com/blog/cara-menghitung-selisih-waktu-dengan-fungsi-date-diff-php -->
-                                                    <span class="time">5 minutes ago</span>
-                                                </div>
-                                            </a>
-                                            <a href="#">
-                                                <div class="notif-img">
-                                                    <img src="/admin/assets/img/chadengle.jpg" alt="Img Profile" />
-                                                </div>
-                                                <div class="notif-content">
-                                                    <span class="subject">Chad</span>
-                                                    <span class="block"> Ok, Thanks ! </span>
-                                                    <span class="time">12 minutes ago</span>
-                                                </div>
-                                            </a>
-                                            <a href="#">
-                                                <div class="notif-img">
-                                                    <img src="/admin/assets/img/mlane.jpg" alt="Img Profile" />
-                                                </div>
-                                                <div class="notif-content">
-                                                    <span class="subject">Jhon Doe</span>
-                                                    <span class="block">
-                                                        Ready for the meeting today...
-                                                    </span>
-                                                    <span class="time">12 minutes ago</span>
-                                                </div>
-                                            </a>
-                                            <a href="#">
-                                                <div class="notif-img">
-                                                    <img src="/admin/assets/img/talha.jpg" alt="Img Profile" />
-                                                </div>
-                                                <div class="notif-content">
-                                                    <span class="subject">Talha</span>
-                                                    <span class="block"> Hi, Apa Kabar ? </span>
-                                                    <span class="time">17 minutes ago</span>
-                                                </div>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <a class="see-all" href="/admin/pesan">See all messages<i class="fa fa-angle-right"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <!-- Notification -->
-                        <li class="nav-item dropdown hidden-caret">
-                            <a class="nav-link dropdown-toggle" href="#" id="notifDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fa fa-bell"></i>
-                                <span class="notification">4</span>
-                            </a>
-                            <ul class="dropdown-menu notif-box animated fadeIn" aria-labelledby="notifDropdown">
-                                <li>
-                                    <div class="dropdown-title">
-                                        You have 4 new notification
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="notif-scroll scrollbar-outer">
-                                        <div class="notif-center">
-                                            <a href="#">
-                                                <div class="notif-icon notif-primary">
-                                                    <i class="fa fa-user-plus"></i>
-                                                </div>
-                                                <div class="notif-content">
-                                                    <span class="block"> New user registered </span>
-                                                    <span class="time">5 minutes ago</span>
-                                                </div>
-                                            </a>
-                                            <a href="#">
-                                                <div class="notif-icon notif-success">
-                                                    <i class="fa fa-comment"></i>
-                                                </div>
-                                                <div class="notif-content">
-                                                    <span class="block">
-                                                        Rahmad commented on Admin
-                                                    </span>
-                                                    <span class="time">12 minutes ago</span>
-                                                </div>
-                                            </a>
-                                            <a href="#">
-                                                <div class="notif-img">
-                                                    <img src="/admin/assets/img/profile2.jpg" alt="Img Profile" />
-                                                </div>
-                                                <div class="notif-content">
-                                                    <span class="block">
-                                                        Reza send messages to you
-                                                    </span>
-                                                    <span class="time">12 minutes ago</span>
-                                                </div>
-                                            </a>
-                                            <a href="#">
-                                                <div class="notif-icon notif-danger">
-                                                    <i class="fa fa-heart"></i>
-                                                </div>
-                                                <div class="notif-content">
-                                                    <span class="block"> Farrah liked Admin </span>
-                                                    <span class="time">17 minutes ago</span>
-                                                </div>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <a class="see-all" href="javascript:void(0);">See all notifications<i class="fa fa-angle-right"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <!-- Quick Actions -->
-                        <li class="nav-item dropdown hidden-caret">
-                            <a class="nav-link" data-toggle="dropdown" href="#" aria-expanded="false">
-                                <i class="fas fa-layer-group"></i>
-                            </a>
-                            <div class="dropdown-menu quick-actions quick-actions-info animated fadeIn">
-                                <div class="quick-actions-header">
-                                    <span class="title mb-1">Quick Actions</span>
-                                    <span class="subtitle op-8">Shortcuts</span>
-                                </div>
-                                <div class="quick-actions-scroll scrollbar-outer">
-                                    <div class="quick-actions-items">
-                                        <div class="row m-0">
-                                            <a class="col-6 col-md-4 p-0" href="#">
-                                                <div class="quick-actions-item">
-                                                    <div class="avatar-item bg-danger rounded-circle">
-                                                        <i class="far fa-calendar-alt"></i>
-                                                    </div>
-                                                    <span class="text">Calendar</span>
-                                                </div>
-                                            </a>
-                                            <a class="col-6 col-md-4 p-0" href="#">
-                                                <div class="quick-actions-item">
-                                                    <div class="avatar-item bg-warning rounded-circle">
-                                                        <i class="fas fa-map"></i>
-                                                    </div>
-                                                    <span class="text">Maps</span>
-                                                </div>
-                                            </a>
-                                            <a class="col-6 col-md-4 p-0" href="#">
-                                                <div class="quick-actions-item">
-                                                    <div class="avatar-item bg-info rounded-circle">
-                                                        <i class="fas fa-file-excel"></i>
-                                                    </div>
-                                                    <span class="text">Reports</span>
-                                                </div>
-                                            </a>
-                                            <a class="col-6 col-md-4 p-0" href="#">
-                                                <div class="quick-actions-item">
-                                                    <div class="avatar-item bg-success rounded-circle">
-                                                        <i class="fas fa-envelope"></i>
-                                                    </div>
-                                                    <span class="text">Emails</span>
-                                                </div>
-                                            </a>
-                                            <a class="col-6 col-md-4 p-0" href="#">
-                                                <div class="quick-actions-item">
-                                                    <div class="avatar-item bg-primary rounded-circle">
-                                                        <i class="fas fa-file-invoice-dollar"></i>
-                                                    </div>
-                                                    <span class="text">Invoice</span>
-                                                </div>
-                                            </a>
-                                            <a class="col-6 col-md-4 p-0" href="#">
-                                                <div class="quick-actions-item">
-                                                    <div class="avatar-item bg-secondary rounded-circle">
-                                                        <i class="fas fa-credit-card"></i>
-                                                    </div>
-                                                    <span class="text">Payments</span>
-                                                </div>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-
                         <!-- Profile -->
                         <li class="nav-item dropdown hidden-caret">
                             <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#" aria-expanded="false">
@@ -305,15 +102,12 @@
                                             </div>
                                             <div class="u-text">
                                                 <h4>Hizrian</h4>
-                                                <p class="text-muted">hello@example.com</p>
-                                                <a href="profile.html" class="btn btn-xs btn-secondary btn-sm">View Profile</a>
+                                                <p class="text-muted">nim</p>
                                             </div>
                                         </div>
                                     </li>
                                     <li>
                                         <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#">My Profile</a>
-                                        <a class="dropdown-item" href="#">Edit Profile</a>
                                         <a class="dropdown-item" href="#">Logout</a>
                                     </li>
                                 </div>
@@ -349,13 +143,8 @@
                             <div class="collapse in" id="collapseExample">
                                 <ul class="nav">
                                     <li>
-                                        <a href="#profile">
-                                            <span class="link-collapse">My Profile</span>
-                                        </a>
-                                    </li>
-                                    <li>
                                         <a href="#edit">
-                                            <span class="link-collapse">Edit Profile</span>
+                                            <span class="link-collapse">Logout</span>
                                         </a>
                                     </li>
                                 </ul>
