84c5a8438d92087304782713407db8563e97df1e
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-17 15:01:23 +0700

    Update Arsip Model

--- app/Models/ArsipModel.php
+++ app/Models/ArsipModel.php
@@ -9,5 +9,5 @@ class ArsipModel extends Model
     protected $table = 'arsip';
     protected $primarykey = 'id';
     protected $useTimestamps = true;
-    protected $allowedFields = ['id_library', 'judul', 'kategori', 'foto', 'link', 'created_at', 'download', 'updated_at'];
+    protected $allowedFields = ['id_library', 'judul', 'kategori', 'foto', 'namafile', 'created_at', 'updated_at', 'downloads'];
 }
