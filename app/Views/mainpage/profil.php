98e68adb57cea2f806561034bd5610d5d9f5b47d
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-25 21:15:34 +0700

    edit Profile Page

--- app/Views/mainpage/profil.php
+++ app/Views/mainpage/profil.php
@@ -18,7 +18,7 @@
 	<div class="container">
 		<div class="body__section">
 			<div class="title__section text-center">
-				<h3>Lorem, ipsum dolor, sit amet consectetur adipisicing elit.</h3>
+				<h3>Himpunan Mahasiswa Teknik Lingkungan Universitas Diponegoro</h3>
 			</div>
 
 			<div class="row">
@@ -28,8 +28,7 @@
 							<div class="img__sec" style="background-image: url(/mainpage/images/jumbotron-profil.jpg);"></div>
 						</div>
 						<p class="mt-3 mb-2 title__linkfil">Himpunan Mahasiswa Teknik Lingkungan</p>
-						<p style="font-size: 14px; color: rgba(0, 0, 0, .8);">Lorem ipsum dolor sit amet
-							consectetur, adipisicing elit. Sit corporis quaerat perferendis.</p>
+						<p style="font-size: 14px; color: rgba(0, 0, 0, .8);">Himpunan Mahasiswa Teknik Lingkungan Universitas Diponegoro yang disingkat HMTL Undip merupakan organisasi mahasiswa yang dibentuk dan bernaung di Program Studi Teknik Lingkungan, Fakultas Teknik Universitas Diponegoro.</p>
 					</a>
 				</div> <!-- end col -->
 				<div class="col-12 col-sm-4">
@@ -38,8 +37,7 @@
 							<div class="img__sec" style="background-image: url(/mainpage/images/jumbotron-profil-biro.jpg);"></div>
 						</div>
 						<p class="mt-3 mb-2 title__linkfil">Biro Teknik Lingkungan</p>
-						<p style="font-size: 14px; color: rgba(0, 0, 0, .8);">Lorem ipsum dolor sit amet
-							consectetur, adipisicing elit. Sit corporis quaerat perferendis.</p>
+						<p style="font-size: 14px; color: rgba(0, 0, 0, .8);">Unit kerja yang mewadahi kegiatan mahasiswa di bidang minat (Ikatan Pecinta Alam), keilmuan (Kelompok Studi Lingkungan), dan keagamaan (Hayatun Nufus).</p>
 					</a>
 				</div> <!-- end col -->
 				<div class="col-12 col-sm-4">
@@ -48,8 +46,7 @@
 							<div class="img__sec" style="background-image: url(/mainpage/images/jumbotron-profil-ukm.jpg);"></div>
 						</div>
 						<p class="mt-3 mb-2 title__linkfil">Unit Kegiatan Mahasiswa</p>
-						<p style="font-size: 14px; color: rgba(0, 0, 0, .8);">Lorem ipsum dolor sit amet
-							consectetur, adipisicing elit. Sit corporis quaerat perferendis.</p>
+						<p style="font-size: 14px; color: rgba(0, 0, 0, .8);">Wadah penyaluran minat, bakat, dan kegemaran mahasiswa Teknik Lingkungan dalam bidang olahraga (futsal, voli, badminton, basket, dan e-sport) dan seni (saman, envoice, dan akustik).</p>
 					</a>
 				</div> <!-- end col -->
 			</div> <!-- end rows -->
